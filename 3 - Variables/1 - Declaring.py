a7655bbdd5462836a9e53130971b5f9609902614
Venti1707 <nickohse3@gmail.com>
2025-02-13 13:53:53 +0800

    Create 1 - Declaring.py

---
+++ 3 - Variables/1 - Declaring.py
@@ -0,0 +1,14 @@
+# There is no command for declaring variables in Python.
+
+# Variables are created the moment a value is assigned to it.
+x = 5
+y = "y"
+print(x)
+print(y)
+
+
+# Variables do not need to be declared with their type.
+# Their types can change even after they have been defined
+z = 4
+z = "z"
+print(z)
