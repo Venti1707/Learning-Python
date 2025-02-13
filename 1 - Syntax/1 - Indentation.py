a71386f511b747bb49ef387b640682fb720fa3bc
Venti1707 <nickohse3@gmail.com>
2025-02-13 13:53:10 +0800

    Create 1 - Indentation.py

---
+++ 1 - Syntax/1 - Indentation.py
@@ -0,0 +1,14 @@
+# Indentation is used to indicate a block of code
+
+# This code would run...
+if 5 > 2:
+    print("5 is greater than 2.")
+
+# but this code would not.
+if 5 > 2:
+print("5 is greater than 2.")
+
+# The number of spaces has to be the same for each block of code.
+if 5 > 2:
+    print("5 is greater than 2...")
+print("which means that 2 is smaller than 5.")
