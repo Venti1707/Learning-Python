227a60a41db3ee79dc353a4dd770bb97deb20b63
Venti1707 <nickohse3@gmail.com>
2025-02-13 22:56:09 +0800

    Update 2 - Int.py

--- 5 - Numbers/2 - Int.py
+++ 5 - Numbers/2 - Int.py
@@ -1,7 +1,7 @@
 # Int is short for integers and are whole numbers, both positive and negative.
 # They cannot have decimal points and can have unlimited length.
 
-a = 1
+a = 161803398874989
 b = 314159265358979323846264338327
 c = -2718281828459045235360287471352
 
