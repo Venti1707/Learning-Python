8a855eedc87165da5e7fdfe4b36fb3fc27698541
Venti1707 <nickohse3@gmail.com>
2025-02-13 23:06:11 +0800

    Update 9 - GlobalAndLocalVariables.py

--- 3 - Variables/9 - GlobalAndLocalVariables.py
+++ 3 - Variables/9 - GlobalAndLocalVariables.py
@@ -1,6 +1,5 @@
 # Global variables are created outside functions.
 # They can be used inside & outside functions.
-
 a = "Global"
 
 def function1():
@@ -9,7 +8,6 @@ def function1():
 function1()
 
 # If a variable is declared within a function, it's known as a local variable
-
 def function2():
     a = "Local"
     print(a)
@@ -17,7 +15,6 @@ def function2():
 function2()
 
 # To create a global function within a function, the global keyword can be used
-
 def function3():
     global c
     c = "Global within a function"
