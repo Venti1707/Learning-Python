e4e636ab38c003469eb33bf43d102bec69801616
Venti1707 <nickohse3@gmail.com>
2025-02-13 23:08:10 +0800

    Create 3 - Float.py

---
+++ 5 - Numbers/3 - Float.py
@@ -0,0 +1,21 @@
+# Float is short for floating point number.
+# They can contain one or more decimals.
+
+a = 1.61803398874989
+b = 2.718281828459045235360287471352
+c = -3.14159265358979323846264338327
+
+print(type(a))
+print(type(b))
+print(type(c))
+
+# Floats can also be scientific numbers with an "e"/"E".
+# This represents that the number has been raised to the power of 10.
+
+d = 1e61
+e = 2E71
+f = -3.1e41
+
+print(type(d))
+print(type(e))
+print(type(f))
