071a96cfc078bcc57a72b64e54f3e50cad1e411c
Venti1707 <nickohse3@gmail.com>
2025-02-13 22:53:50 +0800

    Create 2 - Int.py

---
+++ 5 - Numbers/2 - Int.py
@@ -0,0 +1,10 @@
+# Int is short for integers and are whole numbers, both positive and negative.
+# They cannot have decimal points and can have unlimited length.
+
+a = 1
+b = 314159265358979323846264338327
+c = -2718281828459045235360287471352
+
+print(type(a))
+print(type(b))
+print(type(c))
