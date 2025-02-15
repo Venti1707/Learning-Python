15a84248703b6e2728f489f337446a612e374881
Venti1707 <nickohse3@gmail.com>
2025-02-15 14:50:45 +0800

    Update 3 - Float.py

--- 5 - Numbers/3 - Float.py
+++ 5 - Numbers/3 - Float.py
@@ -1,6 +1,5 @@
 # Float is short for floating point number.
 # They can contain one or more decimals.
-
 a = 1.61803398874989
 b = 2.718281828459045235360287471352
 c = -3.14159265358979323846264338327
