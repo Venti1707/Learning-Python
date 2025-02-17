bcb87734d0ec9661c867dae7cfd01f7b9df72589
Venti1707 <nickohse3@gmail.com>
2025-02-17 22:02:03 +0800

    Update 6 - Random.py

--- 5 - Numbers/6 - Random.py
+++ 5 - Numbers/6 - Random.py
@@ -1,6 +1,6 @@
-# There is no function called random() to generate random numbers.
-# We have to use the built in module named random to generate them instead.
+# There is no function called random() to generate random numbers
+# We have to use the built in module named random to generate them instead
 import random
-lowerBound = 1 # The minimum value it would generate.
-upperBound = 10 # The (maximum value + 1) it would generate.
-print(random.randrange(lowerBound, upperBound))
+lowerBound = 1 # The minimum value it would generate
+upperBound = 10 # The maximum value + 1 it would generate
+print(random.randrange(lowerBound, upperBound)) # Generate a random number between 1 to 9
