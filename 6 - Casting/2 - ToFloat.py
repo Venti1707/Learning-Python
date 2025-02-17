d8d9c9f46abc353319c1477f8442b925f3459a11
Venti1707 <nickohse3@gmail.com>
2025-02-17 22:14:47 +0800

    Update 2 - ToFloat.py

--- 6 - Casting/2 - ToFloat.py
+++ 6 - Casting/2 - ToFloat.py
@@ -4,8 +4,8 @@
 # 3. String literal (Providing the string represents a float or an integer)
 
 a = float(1) # a will be 1.0
-b = float(2.8) # b will be 2.8
-c = float("3") # c will be 3.0
+b = float(2.718281828459045) # b will be 2.718281828459045
+c = float("-3.141592653589793") # c will be -3.141592653589793
 d = float("4.2") # d will be 4.2
 
 print(a)
