06849d78f7ceaae9c59eb62f829aab05b3b4c34a
Venti1707 <nickohse3@gmail.com>
2025-02-17 21:37:19 +0800

    Create 1 - ToInt.py

---
+++ 6 - Casting/1 - ToInt.py
@@ -0,0 +1,14 @@
+# int()
+# 1. Constructs an integer number from an integer literal.
+# 2. A float literal (by removing all decimals).
+# 3. A string literal (providing the string represents a whole number).
+a = int(1)   # a will be 1
+b = int(2.7) # b will be 2
+c = int("3") # c will be 3
+
+print(a)
+print(type(a))
+print(b)
+print(type(b))
+print(c)
+print(type(c))
