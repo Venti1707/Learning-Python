a7abca6e5935462f874b1cbaf2239309035d6707
Venti1707 <nickohse3@gmail.com>
2025-02-17 22:59:46 +0800

    Update 3 - SingleLineVariables.py

--- 7 - Strings/3 - SingleLineVariables.py
+++ 7 - Strings/3 - SingleLineVariables.py
@@ -1,3 +1,3 @@
-# Done with the variable name, followed by an equal sign, then the string
+# Done with the variable name, followed by an equal sign, then the string which is surrounded by single or double quotes
 a = "a"
 print(a)
