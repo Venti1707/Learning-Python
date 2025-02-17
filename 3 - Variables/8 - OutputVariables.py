5e4651d927a8f7fe373387a1949e3deabc7e02ef
Venti1707 <nickohse3@gmail.com>
2025-02-17 21:48:00 +0800

    Update 8 - OutputVariables.py

--- 3 - Variables/8 - OutputVariables.py
+++ 3 - Variables/8 - OutputVariables.py
@@ -1,18 +1,18 @@
-# The print() function is used to output variables.
+# The print() function is used to output variables
 a = "a"
 print(a)
 
-# To output multiple variables, you can either use commas (,)...
+# To output multiple variables, you can either use commas (,)
 b = "b"
 c = "c"
 d = "d"
-print(b, c, d) # The strings here are not joined together as there is a space between the commas.
+print(b, c, d) # The strings here are not joined together as there is a space between the commas
 
-# or the + operator.
+# You can also use the + operator
 e = "e"
 f = "f"
 g = "g"
-print(e + f + g) # The strings here are joined together as there is no space separating them.
+print(e + f + g) # The strings here are joined together as there is no space separating them
 
-# When using the + operator, combining strings and numbers together produces an error.
-# Hence, it is recommended to use commas when combining strings.
+# When using the + operator, combining strings and numbers together produces an error
+# Hence, it is recommended to use commas when combining strings
