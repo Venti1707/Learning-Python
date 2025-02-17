ef48bea096574b7cef2261a38dc16131b3208308
Venti1707 <nickohse3@gmail.com>
2025-02-17 21:46:37 +0800

    Update 7 - AssigningMultipleValues.py

--- 3 - Variables/7 - AssigningMultipleValues.py
+++ 3 - Variables/7 - AssigningMultipleValues.py
@@ -1,5 +1,5 @@
 # Multiple values to multiple variables
-# The number of variables must match the number of values, or else you will get an error.
+# The number of variables must match the number of values, or else you will get an error
 a, b, c = "a", "b", "c"
 print(a)
 print(b)
@@ -12,7 +12,7 @@ print(e)
 print(f)
 
 # Unpacking a collection
-# The process of extracting the collection of values in a list/tuple is called unpacking.
+# The process of extracting the collection of values in a list/tuple is called unpacking
 fruits = ["Apple", "Banana", "Cherry"]
 g, h, i = fruits
 print(g)
