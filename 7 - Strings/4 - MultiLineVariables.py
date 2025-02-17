ff0cc074983284960111e742953abcaf9dcf9f2f
Venti1707 <nickohse3@gmail.com>
2025-02-17 22:59:47 +0800

    Create 4 - MultiLineVariables.py

---
+++ 7 - Strings/4 - MultiLineVariables.py
@@ -0,0 +1,6 @@
+# Done with the variable name, followed by an equal sign, then the string which is surrounded by 3 single or 3 double quotes
+lorem = """Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."""
+print(lorem)
