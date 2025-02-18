31e02bc94ed9d63d7843567616f4ae40071b877b
Venti1707 <nickohse3@gmail.com>
2025-02-18 16:18:01 +0800

    Create 12 - EscapeCharacters.py

---
+++ 7 - Strings/12 - EscapeCharacters.py
@@ -0,0 +1,14 @@
+# Escape characters are a backslash (\) followed by the character you want to insert
+# For instance, to add double quotes to a string which is defined with double quotes as well
+print("TARDIS stands for \"Time And Relative Dimension In Space\"")
+
+# The following is a list of escape characters in Python
+# \' - Single Quote
+# \\ - Backslash
+# \n - New Line
+# \r - Carriage Return
+# \t - Tab
+# \b - Backspace
+# \f - Form Feed
+# \ooo - Octal value
+# \xhh - Hex value
