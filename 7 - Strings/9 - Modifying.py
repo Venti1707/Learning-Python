8169ba1349c617e72f4d0eacc312687de6d929e6
Venti1707 <nickohse3@gmail.com>
2025-02-18 14:58:28 +0800

    Create 9 - Modifying.py

---
+++ 7 - Strings/9 - Modifying.py
@@ -0,0 +1,21 @@
+# Upper case
+# Done with the upper() method
+a = "Hello world!"
+print(a.upper())
+
+# Lower case
+# Done with the lower() method
+print(a.lower())
+
+# Removing whitespace
+# Done with the strip() method
+b = "Hello world! "
+print(b.strip())
+
+# Replacing strings with other strings
+# Done with the replace() method
+print(a.replace("H", "J"))
+
+# Splitting strings into a list
+# Done with the split() method
+print(a.split(" "))
