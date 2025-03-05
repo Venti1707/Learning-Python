450a3b52fc590528a4738031c0d63f67d3d0c6f3
Venti1707 <nickohse3@gmail.com>
2025-03-05 21:21:02 +0800

    Create 1 - Arithmetic.py

---
+++ 9 - Operators/1 - Arithmetic.py
@@ -0,0 +1,31 @@
+# These operators are used with numeric values to perform common mathematical operations
+a = 22
+b = 7
+
+# 1) Addition (+)
+print("Addition")
+print(a + b)
+
+# 2) Subtraction (-)
+print("Subtraction")
+print(a - b)
+
+# 3) Multiplication (*)
+print("Multiplication")
+print(a * b)
+
+# 4) Division (/)
+print("Division")
+print(a / b)
+
+# 5) Modulus (%) - Gives the remainder when a is divided by b
+print("Modulus")
+print(a % b)
+
+# 6) Exponentiation (**)
+print("Exponentiation")
+print(a ** b)
+
+# 7) Floor division (//) - Rounds the result down to the nearest whole number when a is divided by b
+print("Floor division")
+print(a // b)
