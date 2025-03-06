6d0e233a2a45f999b4c1cdf17d62dc78271a18f1
Venti1707 <nickohse3@gmail.com>
2025-03-06 15:05:42 +0800

    Create 3 - Comparison.py

---
+++ 9 - Operators/3 - Comparison.py
@@ -0,0 +1,33 @@
+# These operators are used to compare numerical values and return Boolean values
+a = 22
+b = 7
+
+# 1) variableName == value
+# Returns True when variableName and value are the same
+print("variableName == value")
+print(a == b)
+
+# 2) variableName != value
+# Returns True when variableName and value are not the same
+print("variableName != value")
+print(a != b)
+
+# 3) variableName > value
+# Returns True when variableName is greater than value
+print("variableName > value")
+print(a > b)
+
+# 4) variableName < value
+# Returns True when variableName is lesser than value
+print("variableName < value")
+print(a < b)
+
+# 5) variableName >= value
+# Returns True when variableName is greater than or equal to value
+print("variableName >= value")
+print(a >= b)
+
+# 6) variableName <= value
+# Returns True when variableName is lesser than or equal to value
+print("variableName <= value")
+print(a <= b)
