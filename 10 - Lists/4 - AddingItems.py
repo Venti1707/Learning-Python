582b48b0fd89cbcf49ca6e85e8051a2694cf8720
Venti1707 <nickohse3@gmail.com>
2025-03-11 14:39:54 +0800

    Update 4 - AddingItems.py

--- 10 - Lists/4 - AddingItems.py
+++ 10 - Lists/4 - AddingItems.py
@@ -1,6 +1,6 @@
 a = ["Apple", "Banana", "Cherry"]
 
-# To add an item at the end of a list, use the append method
+# To add an item at the end of a list, use the append() method
 # The append method takes in 1 argument
 # 1) value; The value of the new item
 print("Adding a new element to the end of list a with the append method")
@@ -8,7 +8,7 @@ print(a)
 a.append("Durian")
 print(a)
 
-# To add an item at the a specific index in a list, use the insert method
+# To add an item at the a specific index in a list, use the insert() method
 # The append method takes in 2 argument
 # 1) index; The index of the new item
 # 2) value; The value of the new item
@@ -17,7 +17,7 @@ print(a)
 a.insert(4, "Elderberry")
 print(a)
 
-# To append items from 1 list to another, use the extend method
+# To append items from 1 list to another, use the extend() method
 # The extend method takes in 1 argument
 # 1) iterable; Any iterable object (Tuples, sets, dictionaries)
 print("Adding items from other iterables")
