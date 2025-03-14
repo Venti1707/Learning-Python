d558db6a14cc2cc6cbd796be78e77aa6f212dcb1
Venti1707 <nickohse3@gmail.com>
2025-03-14 21:24:56 +0800

    Create 6 - Joining.py

---
+++ 11 - Tuples/6 - Joining.py
@@ -0,0 +1,10 @@
+# To join 2 tuples together, use the + operator
+a = ("Apple",)
+b = ("Banana", "Cherry")
+c = a + b
+print("Joining tuple a and tuple b together with the + operator")
+print(c)
+
+# To duplicate the content of a tuple, use the * operator
+print("Duplicating the content of tuple c with the * operator")
+print(c * 2)
