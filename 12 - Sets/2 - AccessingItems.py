bcbb51910294a37b73400a891f2ce3c57f61a7a8
Venti1707 <nickohse3@gmail.com>
2025-03-15 16:00:33 +0800

    Create 2 - AccessingItems.py

---
+++ 12 - Sets/2 - AccessingItems.py
@@ -0,0 +1,14 @@
+a = {"Apple", "Banana", "Cherry", "Durian", "Elderberry", "Fig"}
+
+# To get each item in a set, use a for loop
+print("Getting each item in a set using a for loop")
+for item in a:
+    print(item)
+
+# To check if an item is in a set, use the in keyword
+print("Checking if an item is in a set using the in keyword")
+print("Elderberry" in a)
+
+# To check if an item is not in a set, use the not in keyword
+print("Checking if an item is not in a set using the not in keyword")
+print("Grapes" not in a)
