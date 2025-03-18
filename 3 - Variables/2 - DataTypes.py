3b3b4c71b49c0433726c604ed5d9acacaa061ab6
Venti1707 <nickohse3@gmail.com>
2025-03-18 08:56:18 +0800

    Delete 2 - DataTypes.py

--- 3 - Variables/2 - DataTypes.py
+++
@@ -1,9 +0,0 @@
-# If the data type of a variable, you can cast them
-a = str(3) # a will be '3'
-b = int(3) # b will be 3
-c = float(3) # c will be 3.0
-
-# The data type of a variable can be gotten with the type() function
-print(type(a))
-print(type(b))
-print(type(c))
