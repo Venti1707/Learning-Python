6eb730697a466117d8e35d600c9e57eab0751f2f
Venti1707 <nickohse3@gmail.com>
2025-03-18 11:07:12 +0800

    Delete 2 - QuotesInsideQuotes.py

--- 7 - Strings/2 - QuotesInsideQuotes.py
+++
@@ -1,4 +0,0 @@
-# You can use quotes inside a string, as long as they don't match the quotes surrounding the string
-print("It's fine")
-print('TARDIS stands for "Time And Relative Dimension In Space"')
-print('It's not fine')
