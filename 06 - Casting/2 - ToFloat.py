494795714264be823b317a1fa9a2ddc2ebe75878
Venti1707 <nickohse3@gmail.com>
2025-03-18 08:53:52 +0800

    Create 2 - ToFloat.py

---
+++ 06 - Casting/2 - ToFloat.py
@@ -0,0 +1,18 @@
+# float()
+# 1. Constructs a float number from an integer literal
+# 2. A float literal
+# 3. String literal (Providing the string represents a float or an integer)
+
+a = float(1) # a will be 1.0
+b = float(2.718281828459045) # b will be 2.718281828459045
+c = float("-3.141592653589793") # c will be -3.141592653589793
+d = float("4.2") # d will be 4.2
+
+print(a)
+print(type(a))
+print(b)
+print(type(b))
+print(c)
+print(type(c))
+print(d)
+print(type(d))
