5545f4697e06ece5ee0abddaa427b229fadd5c17
Venti1707 <nickohse3@gmail.com>
2025-03-18 08:53:25 +0800

    Create 4 - Names.py

---
+++ 03 - Variables/4 - Names.py
@@ -0,0 +1,19 @@
+# A variable name can only start with a letter or the underscore (_) character
+# A variable name cannot start with a number
+# A variable name can only contain alpha-numeric characters and underscores (a-z, A-Z, 0-9, and _ )
+# Variable names are case-sensitive (age, Age and AGE are three different variables)
+# A variable name cannot be any of the Python keywords
+# Refer to https://www.w3schools.com/python/python_ref_keywords.asp for a list of the Python keywords
+
+# Legal variable names
+myvar = "Variable"
+my_var = "Variable"
+_my_var = "Variable"
+myVar = "Variable"
+MYVAR = "Variable"
+myvar2 = "Variable"
+
+# Illegal variable names
+2myvar = "John"
+my-var = "John"
+my var = "John"
