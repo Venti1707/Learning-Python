4813951a63d298147ae4df01926545dc6e7a815a
Venti1707 <nickohse3@gmail.com>
2025-03-18 08:53:55 +0800

    Create 3 - ToString.py

---
+++ 06 - Casting/3 - ToString.py
@@ -0,0 +1,15 @@
+# str()
+# Constructs a string from a wide variety of data types such as:
+# 1. Strings
+# 2. Integer literals and float literals
+
+a = str("1.61803398874989") # a will be '1.61803398874989'
+b = str(2.718281828459045) # b will be '2.718281828459045'
+c = str(-3.141592653589793) # c will be '-3.141592653589793'
+
+print(a)
+print(type(a))
+print(b)
+print(type(b))
+print(c)
+print(type(c))
