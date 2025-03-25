f342872cfc0527c9878781f9e3e1d6397a8ef70d
Venti1707 <nickohse3@gmail.com>
2025-03-25 14:56:04 +0800

    Create 4 - AddingItems.py

---
+++ 13 - Dictionaries/4 - AddingItems.py
@@ -0,0 +1,19 @@
+a = {
+    "name": "Nic",
+    "favouriteWeapons": [
+        "Elegy of the End",
+        "Crimson Moon's Semblance"
+    ]
+}
+
+# You can add an item to a dictionary by using a new index key and assigning a value to it
+print("Adding the age value to dictionary a")
+print(a)
+a["age"] = 22
+print(a)
+
+# You can also use the update() method which takes in an iterable object with key:value pairs
+print("Using the update() method to update the username key in dictionary a")
+print(a)
+a.update({"username": "Venti1707"})
+print(a)
